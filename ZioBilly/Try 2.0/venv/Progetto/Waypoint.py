f58b4aa5de8d4579b411609ad0b06999b98dff24
Federico <federico@gmail.com>
2019-12-23 22:17:29 +0100

    Creazione classe waypoint con percorsi dritti

---
+++ ZioBilly/Try 2.0/venv/Progetto/Waypoint.py
@@ -0,0 +1,47 @@
+import pygame
+
+
+arrayDritti = [(325, 0), (475, 600), (800, 225), (0, 375)]
+arraySX = [(375, 0), (425, 600), (800, 275), (0, 325)]
+
+class Waypoint():
+	def __init__(self):
+		self.arraySX = []
+		self.arrayDXdri = []
+		self.arrayDXdex = []
+		
+		for x in arrayDritti:
+			print(x[0], x[1])
+			if((x[0] % 2) == 0):
+				if(x[0] == 0):
+					arrayToken = []
+					for i in range(800):
+						arrayToken.append([i, x[1]])
+						print(arrayToken)
+					self.arrayDXdri.append(arrayToken)
+				else:
+					token = x[0]
+					arrayToken = []
+					while token != 0:
+						arrayToken.append([token, x[1]])
+						token = token - 1
+					self.arrayDXdri.append(arrayToken)
+
+			if((x[1] % 2) == 0):
+				if(x[1] == 0):
+					arrayToken = []
+					for i in range(600):
+						arrayToken.append([x[0], i])
+					self.arrayDXdri.append(arrayToken)
+				else:
+					token = x[1]
+					arrayToken = []
+					while token != 0:
+						arrayToken.append([x[0], token])
+						token = token - 1
+					self.arrayDXdri.append(arrayToken)
+
+		
+
+
+Waypoint()
--- ZioBilly/Try 2.0/venv/Progetto/car.py
+++ ZioBilly/Try 2.0/venv/Progetto/car.py
@@ -1,14 +1,21 @@
 import random
 import numpy
 import pygame
+import Waypoint
 
 nord = 1
 sud = 2
 est = 3
 ovest = 4
-direzione = [nord, sud, est, ovest]
+cardinali = [nord, sud, est, ovest]
 velMax = 50
 frena = 0
 
+class Car():
+	super().__init__()
 
-class Veicolo():
+	def __init__(self):
+
+		self.entra = numpy.random.choice(cardinali, p=[0.25, 0.25, 0.25, 0.25])
+		direzione.remove(self.entra)
+		self.direzione = numpy.random.choice("DX", "SX", p = [0.50, 0.50])
